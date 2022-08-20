0b4abfd552896d1a8a665222583fecc599a5548a
Mohammed Badsha Alamgir <BADSHA.BDMAP@GMAIL.COM>
2022-08-21 02:09:33 +0600

    Initial Commit

---
+++ Currency/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Currency
+//
+//  Created by Mohammed Badsha Alamgir on 21/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Currency/CurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyApp.swift
+//  Currency
+//
+//  Created by Mohammed Badsha Alamgir on 21/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
